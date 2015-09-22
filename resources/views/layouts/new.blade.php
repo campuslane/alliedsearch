f021be8d5dbcd8318def35c6549ce7830b476855
frankmatull <frankm@campuslane.com>
2015-09-22 16:59:00 -0700

    added footer top margins

--- resources/views/layouts/new.blade.php
+++ resources/views/layouts/new.blade.php
@@ -5,6 +5,12 @@
 	<title>@yield('title')</title>
 	<link rel="stylesheet" href="/css/theme.css">
 	@yield('styles')
+
+	<style>
+		.site-footer h4 {
+			margin-top:12px;
+		}
+	</style>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
